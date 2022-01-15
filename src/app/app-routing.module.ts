1e982c0904276d633014a0e27acb9579bbb071a3
shindeanant <shindeanant1997@gmail.com>
2022-01-15 22:04:08 +0530

    Added: Ability to create header and dashboard file.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddAddressComponent } from './component/add-address/add-address.component';
+import { HeaderComponent } from './component/header/header.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashBoardComponent },
+  { path: 'dashboard', component: DashBoardComponent },
+  {path:'addcontact', component:AddAddressComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { AddAddressComponent } from './component/add-address/add-address.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DashBoardComponent,
+    AddAddressComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
